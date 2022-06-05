91f2ce06c2572fd2829a6b39c2cd1dc83e525379
熊猫 <2443005004@qq.com>
2022-06-05 14:36:49 +0800

    添加reamde

---
+++ README.md
@@ -0,0 +1,10 @@
+# webpack5_vue3_ts_cli
+
+**介绍**
+使用webpack5搭建的vue3 + typescript 开发脚手架
+
+**功能**
+
+- 使用 babel 打包 typescript ，配置js向后兼容
+- 使用 sass 预处理器
+- 使用 postcss ，配置css向后兼容
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// 这是全局样式文件，必须要引入，否则会有样式丢失
 import "element-plus/dist/index.css";
 
 const app = createApp(App);
